254791a36e83c8a0a4b9b0fa2a37424ed2e676ad
Haybuka <paschal.nduleo@gmail.com>
2021-09-24 10:50:40 +0800

    added mobile view layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 let modeToggle = document.querySelector('.togglerMode');
+let harmburger = document.querySelector('.harmburger');
+let navList = document.querySelector('header ul');
 let body = document.querySelector('body');
+
 modeToggle.addEventListener('click',function(){
    body.classList.toggle('dark') 
+})
+
+harmburger.addEventListener('click',()=>{
+   navList.classList.toggle('show')
 })
